d43db1c0225117b95e2431ca5bd4b5306117b28d
Sunskimmer82 <92886026+Sunskimmer822@users.noreply.github.com>
2023-02-22 16:50:35 -0800

    make master sword actually work (oops)

--- src/main/java/net/serenas/botm/MasterSword.java
+++ src/main/java/net/serenas/botm/MasterSword.java
@@ -1,7 +1,11 @@
 package net.serenas.botm;
 
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+
 public class MasterSword extends SwordItem {
 
     public MasterSword(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
-	            super(toolMaterial, attackDamage, attackSpeed, settings);
+	    super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
 }
